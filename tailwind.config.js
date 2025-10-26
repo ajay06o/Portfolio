f11efa6013b2274ae792fce90339ff35e4439499
ajay06o <ajayprabhuraju24@gmail.com>
2025-10-26 22:07:36 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+      'auto': 'repeat(auto-fit, minmax(250px, 1fr))',
+    }, 
+    fontFamily: {
+      'outfit': ['Outfit', 'sans-serif'],
+      'ovo': ['Ovo', 'serif'],
+    },
+    animation: {
+      'spin': 'spin 6s linear infinite',
+    },
+    colors: {
+      lightHover: '#fcf4ff',
+      darkHover: '#2a004a',
+      darkTheme: '#11001F',
+    },
+    boxShadow: {
+      'black': '4px 4px 0 #000',
+      'white': '4px 4px 0 #fff',
+  }
+}
+  },
+  plugins: [],
+     darkMode: 'selector'
+}
